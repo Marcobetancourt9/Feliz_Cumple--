c582e9132eb8197ab61470c522629e49e32d1ee4
Marcobetancourt9 <marco.betancourt@correo.unimet.edu.ve>
2025-10-24 16:14:42 -0400

    Eres la mejor novia del mundio

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var w = c.width = window.innerWidth,
 		hh = h / 2,
 		
 		opts = {
-			strings: [ 'FELIZ', 'CUMPLEAÑOS','NOVIA', 'ERES LA MEJOR NOVIA DEL MUNDO', 'TE AMOOOO ❤️'],
+			strings: [ 'FELIZ', 'CUMPLEAÑOS','NOVIA', 'ERES', 'LA MEJOR NOVIA','DEL MUNDO', 'TE AMOOOO ❤️'],
 			charSize: 30,
 			charSpacing: 35,
 			lineHeight: 40,
